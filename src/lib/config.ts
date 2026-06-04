c2a37d1a1bc58fa713e91e6c3d9baf006d4a06a1
falase femi <femifalase228@gmail.com>
2026-06-04 03:20:15 +0100

    remove better auth

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,8 +4,6 @@ import { ConfigError } from "../lib/errors.ts";
 export class EnvConfig extends Schema.Class<EnvConfig>("EnvConfig")({
 	DATABASE_URL: Schema.String,
 	PORT: Schema.String,
-	BETTER_AUTH_SECRET: Schema.String,
-	BETTER_AUTH_URL: Schema.String,
 }) {}
 
 export const loadConfig = Schema.decodeUnknownExit(EnvConfig)(process.env).pipe(
